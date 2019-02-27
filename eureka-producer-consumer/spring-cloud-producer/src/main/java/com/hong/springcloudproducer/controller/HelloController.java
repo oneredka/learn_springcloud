967de929935e0b4f387b9756f1e08c3677993634
yihong <gg_hilock@126.com>
2019-02-27 16:49:45 +0800

    服务提供与调用

---
+++ eureka-producer-consumer/spring-cloud-producer/src/main/java/com/hong/springcloudproducer/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.hong.springcloudproducer.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName HelloController
+ * @Description TODO
+ * @Author hong
+ * @Date 2019/2/27 16:09
+ **/
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String index(@RequestParam String name) {
+        return "hello "+name+"，this is first messge";
+    }
+}
