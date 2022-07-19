0dc40d9650bed1478c72033e211ffa9208f62476
LouisHongYi <louis.yi@qima.com>
2022-07-19 12:39:09 +0800

    test config file

--- spring-cloud-nacos-config/src/main/java/com/example/springcloudnacos/SpringCloudNacosApplication.java
+++ spring-cloud-nacos-config/src/main/java/com/example/springcloudnacos/SpringCloudNacosApplication.java
@@ -1,13 +1,38 @@
 package com.example.springcloudnacos;
 
+import org.apache.tomcat.jni.Time;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.context.ConfigurableWebApplicationContext;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class SpringCloudNacosApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringCloudNacosApplication.class, args);
+    public static void main(String[] args) throws InterruptedException {
+        //SpringApplication.run(SpringCloudNacosApplication.class, args);
+
+        /**
+         * 手动在nacos 上添加配置
+         * user.name=nacos-config-properties
+         * user.age=90
+         */
+       /* ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringCloudNacosApplication.class, args);
+        String  userName = applicationContext.getEnvironment().getProperty("user.name");
+        String  userAge = applicationContext.getEnvironment().getProperty("user.age");
+        System.out.println("user name: " + userName + ", user age: " + userAge);*/
+
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringCloudNacosApplication.class, args);
+        while (true) {
+            String userName = applicationContext.getEnvironment().getProperty("user.name");
+            String userAge = applicationContext.getEnvironment().getProperty("user.age");
+            System.err.println("user name: " + userName + ", user age: " + userAge);
+            System.err.println("extension config:" + applicationContext.getEnvironment().getProperty("parttern.name"));
+            TimeUnit.SECONDS.sleep(1);
+        }
+
     }
 
 }
